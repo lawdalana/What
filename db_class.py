893e6ea2336b3d745ea58ac251f177600318c0f9
LawD <wasawat@dek-d.com>
2017-06-13 18:40:30 +0700

    Auto_kmean V2.0

--- db_class.py
+++ db_class.py
@@ -1,40 +1,41 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 import pymysql
-
-
-# In[2]:
-
 
 class db:
     
-    def __init__(self, hostname, port, username, password, databasename):
-        self.Connection = pymysql.connect(host=hostname, port=port, user=username, password=password, 
-             db=databasename ,charset='utf8mb4' ,cursorclass=pymysql.cursors.DictCursor )
-        self.cursor = self.Connection.cursor()
-    
-    def __del__(self):
-        self.cursor.close()
-        self.Connection.close()
-    
-    def find(self, statement):
-        self.cursor.execute(statement)
-        buf = list()
-        for row in self.cursor.fetchall():
-            buf.append(row)
-        return buf
-    
-    def Operation(self, statement):
-        try:
-           # Execute the SQL command
-           self.cursor.execute(sql)
-           # Commit your changes in the database
-           self.Connection.commit()
-        except:
-           # Rollback in case there is any error
-           self.Connection.rollback()
+  def __init__(self, hostname, port, username, password, databasename):
+    self.Connection = pymysql.connect(host=hostname, port=port, user=username, password=password, 
+         db=databasename ,charset='utf8mb4' ,cursorclass=pymysql.cursors.DictCursor )
+    self.cursor = self.Connection.cursor()  
 
+  def __del__(self):
+    self.cursor.close()
+    self.Connection.close()
+
+  def find(self, statement):
+    self.cursor.execute(statement)
+    buf = list()
+    for row in self.cursor.fetchall():
+        buf.append(row)
+    return buf
+
+  def CURD(self, statement):
+    try:
+       # Execute the SQL command
+      self.cursor.execute(statement)
+       # Commit your changes in the database
+      self.Connection.commit()
+    except:
+      print("Error" + statement)
+       # Rollback in case there is any error
+      self.Connection.rollback() 
+
+  def CURD_SP(self, statement, title):
+    try:
+       # Execute the SQL command
+      self.cursor.execute(statement,(title))
+       # Commit your changes in the database
+      self.Connection.commit()
+    except:
+      print("Error" + statement)
+       # Rollback in case there is any error
+      self.Connection.rollback() 
